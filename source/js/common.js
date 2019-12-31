ff8667fa112e9bf1ce1e86c52dfa18867f5a6143
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2019-12-31 02:49:18 -0500

    Финальные правки

--- source/js/common.js
+++ source/js/common.js
@@ -1,9 +1,11 @@
 var navMain = document.querySelector('.nav');
-var navToggle = document.querySelector('.nav__toggle');
+var navToggle = navMain.querySelector('.nav__toggle');
 
 navMain.classList.remove('nav--nojs');
+navMain.classList.add('nav--closed');
+navMain.classList.remove('nav--opened');
 
-navToggle.addEventListener('click', function () {
+navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('nav--closed')) {
     navMain.classList.remove('nav--closed');
     navMain.classList.add('nav--opened');
